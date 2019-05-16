de7e75785b40c2fdd890015f4e7d1d3e105053c8
Zied <guesmy.zied@gmail.com>
2019-05-16 11:58:01 +0100

    Update docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-unzip /iexec_in/$DATASET_FILENAME -d /iexec_in && \
-      rm /iexec_in/$DATASET_FILENAME
-
+unzip   /iexec_in/$DATASET_FILENAME -d /iexec_in    && \
+rm      /iexec_in/$DATASET_FILENAME                 && \
 python3 /object_detector.py $@
